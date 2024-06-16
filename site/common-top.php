e6c2558bbb6351803b50b32ffb2b5e17fa09448d
waimea-jmblack <jmblack@waimea.school.nz>
2024-06-17 10:56:16 +1200

    starting code

---
+++ site/common-top.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Demo</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+
+    <header>
+        <h1>PHP Form Demos</h1>
+
+        <nav>
+            <a href="index.php">Home</a>
+            <a href="about.php">About</a>
+        </nav>
+</header>
+            
---
+++ site/index.php
@@ -0,0 +1,7 @@
+<?php include 'common-top.php'; ?>
+
+<main>
+    <h1>Hello Mate</h1>
+</main>
+
+<?php include 'common-bottom.php';
