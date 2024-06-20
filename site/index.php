69f47235fcf27ff4ce2ba254ad350ccd4d37d497
waimea-jmblack <jmblack@waimea.school.nz>
2024-06-20 15:10:10 +1200

    Work

--- site/index.php
+++ site/index.php
@@ -1,55 +1,26 @@
 <?php include 'partials/common-top.php'; ?>
 
-<div id="forms">
+<?php
 
-    <form method="POST" action="process-signup.php">
-        <h2>Sign In Bozo</h2>
+//Connect to database
+$db = connectToDB();
 
-        <label>Name</label>
-        <input name="user" type="text" required>
+//Setup a queary to get all company info
+$query = 'SELECT * FROM Broken Post Sighting';
 
-        <label>Email</label>
-        <input name="mail" type="email" required>
+//Attempt to run the query
+try {
+    $stmt = $db->prepare($query);
+    $stmt->execute();
+    $BrokenPostSighting = $stmt->fetchAll();
+}
+catch (PDOException $e) {
+    consoleLog($e->getMessage(), 'DB List Fetch',ERROR);
+    die('There was an error getting data from the database');
+}
 
-        <label>Password</label>
-        <input name="pass" type="password" required>
+consoleLog($BrokenPostSighting);
 
-        <input type="submit" value="Register">
-    </form>
-</body>
-
-<body>
-
-    <form method="POST" action="process-calc.php">
-        <h2>Add Some Numbers Kid</h2>
-
-        <label>1st Number</label>
-        <input name="num1" type="number" required>
-
-        <label>2nd Number</label>
-        <input name="num2" type="number" required>
-
-        <label>
-            <input name="operator" value="add" type="radio" checked>
-            Add
-        </label>
-
-        <label>    
-            <input name="operator" value="sub" type="radio">
-            Subtract
-        </label>
-
-        <label>
-            <input name="operator" value="mul" type="radio">
-            Multiply
-        </label>
-
-        <label>
-            <input name="operator" value="div" type="radio">
-            Divide
-        </label>
-
-        <input type="submit" value="Mathmatical Answer">
-    </form>
+?>
 
 <?php include 'partials/common-bottom.php';
