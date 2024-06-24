be18bcf7bfb8b438ea0c550c7ec05b2d2f82a54f
waimea-jmblack <jmblack@waimea.school.nz>
2024-06-25 09:54:51 +1200

    Worked on webpage home

---
+++ site/_config.php
@@ -0,0 +1,4 @@
+<?php
+
+define('SITE_NAME', 'PHP Test Site');
+define('SITE_AUTHOR', 'James Black');
--- site/index.php
+++ site/index.php
@@ -1,4 +1,9 @@
-<?php include 'partials/common-top.php'; ?>
+<?php 
+
+require '_functions.php';
+include 'partials/top.php'; 
+
+?>
 
 <?php
 
@@ -6,24 +11,24 @@
 $db = connectToDB();
 
 //Setup a queary to get all company info
-$query = 'SELECT * FROM jmblack_BPdatabase';
+$query = 'SELECT * FROM posts';
 
 //Attempt to run the query
 try {
     $stmt = $db->prepare($query);
     $stmt->execute();
-    $BrokenPostSighting = $stmt->fetchAll();
+    $posts = $stmt->fetchAll();
 }
 catch (PDOException $e) {
     consoleLog($e->getMessage(), 'DB List Fetch',ERROR);
     die('There was an error getting data from the database');
 }
 
-consoleLog($BrokenPostSighting);
+consoleLog($posts);
 
 ?>
 
-<h1>Title</h1>
-<h2>Header</h2>
+<h1>Hi Dad, this is your website/app</h1>
+<h2>This app can be used for your Weather, Soil acidity, Irrigation and keeping track of your Broken Posts</h2>
 
-<?php include 'partials/common-bottom.php';
+<?php include 'partials/bottom.php';
