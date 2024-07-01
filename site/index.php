e6a36c3092a55a760122f7d7f898e0a7ec92d3ec
waimea-jmblack <jmblack@waimea.school.nz>
2024-07-02 09:01:16 +1200

    fin jk

--- site/index.php
+++ site/index.php
@@ -28,24 +28,24 @@ consoleLog($posts);
 
 ?>
 
-<h1>Hi Dad, this is your website/app</h1>
-<h2>This app can be used for your Weather, Soil acidity, Irrigation and keeping track of your Broken Posts</h2>
+<h3>Hi Dad, this is your website/app</h3>
+<h3>This app can be used for your Weather, Soil acidity, Irrigation and keeping track of your Broken Posts</h3>
 
 
-<h1>Simple Mobile-First Responsive Site</h1>
+<h2>Simple Mobile-First Responsive Site</h2>
 
 <p>This is a simple, responsive site that uses minimal CSS to achieve reasonable layout on both mobile and desktop devices...</p>
 
 <section id="top">
 
     <main>
-        <h1>Simple Mobile-First Responsive Site</h1>
+        <h2>Simple Mobile-First Responsive Site</h2>
 
         <p>This is a simple, responsive site that uses minimal CSS to achieve reasonable layout on both mobile and desktop devices...</p>
 
         <section>
             <article>
-                <h2>Some Stuff</h2>
+                <h3>Some Stuff</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Et netus et malesuada fames ac turpis egestas. Tellus at urna condimentum mattis pellentesque id nibh tortor. </p>
             </article>
        
